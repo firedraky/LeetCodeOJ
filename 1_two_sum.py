6326b03cd211f9bebb658b822fc3382fb2abaee1
firedraky <firedraky@sina.com>
2015-08-17 22:04:08 +0800

    for test

---
+++ 1_two_sum.py
@@ -0,0 +1,10 @@
+__author__ = 'hxlcj'
+
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer[]}
+    def twoSum(self, nums, target):
+        pass
+
