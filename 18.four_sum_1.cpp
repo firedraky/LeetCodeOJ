79c87dfc63c29c4bd87fe83d631a5672a7f78696
Chuanjian Liang <firedraky13@gmail.com>
2015-09-07 22:47:04 +0800

    Update 18.four_sum_1.cpp

--- 18.four_sum_1.cpp
+++ 18.four_sum_1.cpp
@@ -57,4 +57,34 @@ vector<vector<int>> fourSum(vector<int>&num,int target){
     result.erase(unique(result.begin(),result.end()),result.end());
     return result;
 }
-*/
+
+vector<vector<int>> fourSum(vector<int>&num,int target){
+    vector<vector<int>> result;
+    if (num.size()<4) return result;
+    sort(num.begin(),num.end());
+    unordered_multimap<int,pair<int,int>> cache;
+    for (int i = 0; i < num.size()-1; ++i) {
+        for (int j = i+1; j < num.size(); ++j) {
+            cache.insert(make_pair(num[i]+num[j],make_pair(i,j)));
+        }
+    }
+
+    for(auto i=cache.begin();i!=cache.end();++i){
+        int x=target - i->first;
+        auto range=cache.equal_range(x);
+        for (auto j = range.first;j!=range.second; ++j) {
+            auto a=i->second.first;
+            auto b=i->second.second;
+            //a<b => num[a]<=num[b]
+            auto c=j->second.first;
+            auto d=j->second.second;
+            //c<d
+            if(c<=b) continue;
+            vector<int> vec{num[a],num[b],num[c],num[d]};
+            result.push_back(vec);
+        }
+    }
+    sort(result.begin(),result.end());
+    result.erase(unique(result.begin(),result.end()),result.end());
+    return result;
+}*/
