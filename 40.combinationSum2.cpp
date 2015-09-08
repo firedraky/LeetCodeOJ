4697941383d337501f33eb7321391dc86fa2aa05
Chuanjian Liang <firedraky13@gmail.com>
2015-09-08 21:31:24 +0800

    Update 40.combinationSum2.cpp

--- 40.combinationSum2.cpp
+++ 40.combinationSum2.cpp
@@ -28,3 +28,28 @@ void findSolution2(int target,const vector<int>& candidates,vector<int>&tempResu
     }
 }
 };
+
+/*
+vector<vector<int> > combinationSum2(vector<int> &candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int> > res;
+        vector<int> combination;
+        combinationSum(candidates, target, res, combination, 0);//递归，找到和为target的所有组合
+        //auto end_unique = unique(res.begin(),res.end());
+        //res.erase(end_unique,res.end());
+        return res;
+}
+private:
+    void combinationSum(vector<int> &candidates, int target, vector<vector<int> > &res, vector<int> &combination, int begin) {
+        if  (!target) {
+            res.push_back(combination);
+            return;
+        }
+        for (int i = begin; i != candidates.size() && target >= candidates[i];++i) {//每循环一次结束，combination置0
+            combination.push_back(candidates[i]);
+            combinationSum(candidates, target - candidates[i], res, combination, i+1);
+            combination.pop_back();
+            while(candidates[i]==candidates[i+1]) i++;
+        }
+    }
+*/
